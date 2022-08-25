eac80007bc5c9fc2fa3294743b13bcfd9293c964
gidits13 <gidits13@gmail.com>
2022-08-25 19:06:32 +0300

    Добавьте файлы проекта.

---
+++ Chapter7p66/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Chapter7p66
+{
+    class Record<T1,T2>
+    {
+        public T1 Id;
+        public T2 Value;
+        public DateTime Date;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
